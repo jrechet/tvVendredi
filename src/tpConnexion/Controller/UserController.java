01dc71adbd88d807e699e5956d183d234943420c
TregMor <TregMor.com>
2018-01-19 16:38:08 +0100

    End of day update

---
+++ src/tpConnexion/Controller/UserController.java
@@ -0,0 +1,48 @@
+package tpConnexion.Controller;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import tpConnexion.User;
+
+@ManagedBean
+@RequestScoped
+public class UserController implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private User user;
+
+	public UserController() {
+		user = new User();
+	}
+
+	public void add() {
+		System.out.println(user.getmail());
+	}
+
+	// @EJB
+	// private
+	//
+	// public void add() {
+	// userDao.add(user);
+	//
+	// }
+
+	public String display() {
+		System.out.println("User connection");
+		System.out.println(user.getmail());
+		System.out.println(user.getPassword());
+		return "display";
+	}
+
+	// --Getter - Setter --
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
---
+++ src/tpConnexion/DAO/UserDao.java
@@ -0,0 +1,20 @@
+package tpConnexion.DAO;
+
+//import javax.ejb.Stateless;
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//
+//import tpConnexion.User;
+//
+//@Stateless
+//public class UserDao {
+//
+//    @PersistenceContext(unitName = "poe-persistence-unit")
+//    private EntityManager em;
+//
+//    public void add(User user) {
+//        System.out.println("em: " + em);
+//        em.persist(user);
+//    }
+
+//}
